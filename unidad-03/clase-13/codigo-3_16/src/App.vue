097730ab6a01476f9f1758b95fa9c67ef0956cab
Rodrigo Tufiño <rtufino@ups.edu.ec>
2025-08-03 21:23:26 -0500

    Código 3.16

---
+++ unidad-03/clase-13/codigo-3_16/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import TarjetaUsuario from './components/TarjetaUsuario.vue';
+</script>
+
+<template>
+  <h1>Ejemplo de Props</h1>
+  <TarjetaUsuario nombre="María Torres" correo="maria@correo.com" />
+</template>
+
+<style scoped></style>
---
+++ unidad-03/clase-13/codigo-3_16/src/components/TarjetaUsuario.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tarjeta">
+    <h2>{{ nombre }}</h2>
+    <p>Email: {{ correo }}</p>
+  </div>
+</template>
+
+<script setup>
+// Declaración explícita de las props esperadas
+defineProps({
+  nombre: String,
+  correo: String
+});
+</script>
+
+<style scoped>
+.tarjeta {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 8px;
+  max-width: 300px;
+  margin: auto;
+  background-color: #f9f9f9;
+}
+</style>
