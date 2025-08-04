f00f04e2e5ad4bc037119fe6dbd9ff26548c69a6
Rodrigo Tufiño <rtufino@ups.edu.ec>
2025-08-03 20:56:20 -0500

    Código 3.15

---
+++ unidad-03/clase-13/codigo-3_15/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Componente personalizado</h1>
+  <p>Total Acumulado: {{ total }}</p>
+  <BotonContador @incrementado="sumar"/>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import BotonContador from './components/BotonContador.vue';
+
+const total = ref(0);
+
+const sumar = () => {
+  total.value++;
+};
+
+</script>
+
+<style scoped></style>
---
+++ unidad-03/clase-13/codigo-3_15/src/components/BotonContador.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="notificarPadre">
+    Haz clic (local: {{ clics }})
+  </button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const clics = ref(0);
+const emit = defineEmits(['incrementado']);
+
+const notificarPadre = () => {
+  clics.value++;
+  emit('incrementado'); // Se emite el evento hacia el componente padre
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.7rem;
+  background-color: steelblue;
+  color: white;
+  font-size: 1rem;
+  border-radius: 6px;
+  cursor: pointer;
+}
+</style>
